24a113dad8611944650f3f44c50d40123b2ea02f
by-Soulhunt <mydronmail@gmail.com>
2025-06-20 11:40:39 +0300

    Commit #1: feat: add method to create full search URL

--- modules/maudau.py
+++ modules/maudau.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import quote
 from modules.products import ReadFile
 
 
@@ -12,12 +13,19 @@ class MaudauParser:
         self.start_url_search = "https://maudau.com.ua/search?text="
         self.response = ''
         self.list_of_products = ReadFile().return_list_of_products
-        self.list_of_search_urls = []
+        self.list_of_search_urls = self.create_list_of_full_urls()
 
 
     def create_list_of_full_urls(self):
-        pass
+        """
+        Create full url path with correct URL-encoding
+        :return: list of search urls
+        """
+        list_of_search_urls = []
+        for url in self.list_of_products:
+            list_of_search_urls.append(self.start_url_search + quote(url))
 
+        return list_of_search_urls
 
     def search(self):
         self.response = requests.get(self.start_url_search)
